92e533a06c929b23c616d2a8d46d681ec3e542bf
Fran Agustín <francokuchiki@gmail.com>
2021-08-17 07:38:43 -0300

    Add support for multiple clients so they can authenticate with different credentials or on different databases

--- mongodb_bundle/bundle.py
+++ mongodb_bundle/bundle.py
@@ -1,16 +1,40 @@
+from typing import Any, Dict, List, Optional, Union
+
 from pymongo import MongoClient
 from pydantic import BaseModel
 from dependency_injector import containers, providers
 from applauncher.applauncher import Configuration
 
 
-class MongoDBConfig(BaseModel):
+class BaseMongoURI(BaseModel):
     uri: str
     connect: bool = True
 
 
+class NamedMongoURI(BaseMongoURI):
+    name: str
+
+
+class MongoDBConfig(BaseMongoURI):
+    uris: Optional[List[NamedMongoURI]]
+
+
 def get_default_database(client: MongoClient):
     return client.get_default_database()
+
+
+class MongoClientsContainer:
+    def __init__(self, named_uris: List[Dict[str, Any]]):
+        self._uris = {u.name: {'uri': u.uri, 'connect': u.connect} for u in named_uris}
+
+    def __getitem__(self, conn_name: str) -> MongoClient:
+        uri = self._uris[conn_name]
+        return MongoClient(host=uri['uri'], connect=uri['connect'])
+
+    def get(self, conn_name: str, default: Any = None) -> Union[MongoClient, Any]:
+        if conn_name not in self._uris:
+            return default
+        return self.__getitem__(conn_name)
 
 
 class MongoDBContainer(containers.DeclarativeContainer):
@@ -25,6 +49,11 @@ class MongoDBContainer(containers.DeclarativeContainer):
     db = providers.Callable(
         get_default_database,
         mongo_client
+    )
+
+    clients = providers.Callable(
+        MongoClientsContainer,
+        named_uris=configuration.provided.mongodb.uris
     )
 
 
